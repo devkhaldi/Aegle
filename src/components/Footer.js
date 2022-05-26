5e1c087589bd7c89734353636b8ade9317e20450
devkhaldi <dev.khaldi@gmail.com>
2022-05-26 18:26:20 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,54 @@
 import React from "react"
-import companyLogo from "../Images/company-logo.svg"
+import companyLogo from "../Icons/footerCompanyLogo.png"
 import gotopIcon from "../Icons/arrow-up_icon.svg"
+import "./Footer.css"
 
 const Footer = () => {
   return (
     <footer>
       <div className='footerContent'>
-        <div className='footerInfo'>
+        <div className='footerIntro'>
           <img src={companyLogo} alt='company logo' />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor
             facilisi tincidunt nibh.
           </p>
-          <p>info@aeglehealthpartners.com</p>
+          <a href='mailto:info@aeglehealthpartners.com'>
+            info@aeglehealthpartners.com
+          </a>
         </div>
         {/* prettier-ignore */}
-        <div className="productLinks">
+        <div className="links">
           <h3>Product</h3>
-          <a href="#" className="footerLink">Log in</a>
-          <a href="#" className="footerLink">Simulate Aegle</a>
-          <a href="#" className="footerLink">What is Aegle</a>
-          <a href="#" className="footerLink">How Aegle Works</a>
+          <a href="#" className="link yellowLink">Log in</a>
+          <a href="#" className="link">Simulate Aegle</a>
+          <a href="#" className="link">What is Aegle</a>
+          <a href="#" className="link">How Aegle Works</a>
         </div>
         {/* prettier-ignore */}
-        <div className="companyLinks">
+        <div className="links">
           <h3>Company</h3>
-          <a href="#" className="footerLink">Our Story</a>
-          <a href="#" className="footerLink">Aegle Team</a>
-          <a href="#" className="footerLink">Careers</a>
-          <a href="#" className="footerLink">Support</a>
-          <a href="#" className="footerLink">Contact Us</a>
+          <a href="#" className="link">Our Story</a>
+          <a href="#" className="link">Aegle Team</a>
+          <a href="#" className="link">Careers</a>
+          <a href="#" className="link">Support</a>
+          <a href="#" className="link yellowLink">Contact Us</a>
         </div>
         {/* prettier-ignore */}
-        <div className="generalConditionsLinks">
+        <div className="links">
           <h3>General Conditions</h3>
-          <a href="#" className="footerLink">Privacy Policy</a>
-          <a href="#" className="footerLink">Terms &amp; Conditions</a>
-          <a href="#" className="footerLink">Cookies Management</a>
+          <a href="#" className="link">Privacy Policy</a>
+          <a href="#" className="link">Terms &amp; Conditions</a>
+          <a href="#" className="link">Cookies Management</a>
         </div>
       </div>
+      <hr />
       <div className='footerCopyright'>
         <span>© 2021 Aegle Health Partners</span>
         <span>4250 N Fairfax Dr, Ste 600, Arlington, VA 22203</span>
       </div>
 
-      <img src={gotopIcon} alt='gotop icon' />
+      <img className='gotop' src={gotopIcon} alt='gotop icon' />
     </footer>
   )
 }
